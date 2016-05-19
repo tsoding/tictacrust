9f24328cbb5c58c835c8439b487bcbed06930164
Akshay Shekher <voldyman666@gmail.com>
2016-05-20 00:15:05 +0530

    better loops and array initialization
    derive the enum from copy and clone to easily instantiate the array and loop with index too!

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,14 @@
 // [7] [8] [9]
 // > 5
 
+#[derive(Copy, Clone)]
 enum Cell {
     Empty,
     Cross,
     Zero
 }
 
-fn print_cell(cell: &Cell, idx: u32) {
+fn print_cell(cell: &Cell, idx: usize) {
     match *cell {
         Cell::Empty => print!("[{}]", idx),
         Cell::Cross => print!(" x "),
@@ -19,20 +20,13 @@ fn print_cell(cell: &Cell, idx: u32) {
 }
 
 fn print_board(board: &[Cell; 9]) {
-    let mut idx = 0;
-    for row in board.chunks(3) {
-        for cell in row {
-            print_cell(cell, idx);
-            idx += 1
-        }
-        println!("")
+    for (idx, cell) in board.iter().enumerate() {
+        print_cell(cell, idx);
     }
 }
 
 fn main() {
-    let board: [Cell; 9] = [Cell::Empty, Cell::Empty, Cell::Empty,
-                            Cell::Empty, Cell::Cross, Cell::Empty,
-                            Cell::Empty, Cell::Empty, Cell::Empty];
+    let board: [Cell; 9] = [Cell::Empty; 9];
 
     print_board(&board);
 }
